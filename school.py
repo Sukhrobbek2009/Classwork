80552d9c067d5541fd393a62a30d52fc2ba309f2
Sam <sukhrobbekrustamov26@gmail.com>
2025-11-12 17:56:38 -0800

    school

---
+++ school.py
@@ -0,0 +1,11 @@
+class school:
+    def __init__(self, student):
+        self.student = student
+
+    def add_student(self, name):
+        self.student.append(name)
+
+    def show_student(self):
+        for student in self.student:
+            print(student)
+student = school(["Sam", "Rob"])
